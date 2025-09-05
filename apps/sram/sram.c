6a8c302cb581cb69d0cc9506600d7e5a0f8d7030
Mike Vengerovich <mikeveng73@gmail.com>
2025-09-05 20:02:14 +0300

    + bug-fix

--- apps/sram/sram.c
+++ apps/sram/sram.c
@@ -74,7 +74,8 @@ int main(void) {
     size_t heap = get_heap_total();
     if (heap > (520 << 10)) {
         fgoutf(f, "\nButter-PSRAM size: %d MB\n", (heap >> 20));
-        uint8_t* p = (uint8_t*)0x11000000;
+        sz = 1024 << 10;
+        p = (uint8_t*)malloc(sz);
         uint32_t begin = time_us_32();
         for (a = 0; a < sz; ++a) {
             p[a] = a & 0xFF;
@@ -83,7 +84,7 @@ int main(void) {
         double d = 1.0;
         double speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "8-bit line write speed: %f MBps\n", speed);
-        if (marked_to_exit) return 0;
+        if (marked_to_exit) goto e;
 
         begin = time_us_32();
         for (a = 0; a < sz; ++a) {
@@ -95,7 +96,7 @@ int main(void) {
         elapsed = time_us_32() - begin;
         speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "8-bit line read speed: %f MBps\n", speed);
-        if (marked_to_exit) return 0;
+        if (marked_to_exit) goto e;
 
         begin = time_us_32();
         for (a = 0; a < sz; a += 2) {
@@ -104,7 +105,7 @@ int main(void) {
         elapsed = time_us_32() - begin;
         speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "16-bit line write speed: %f MBps\n", speed);
-        if (marked_to_exit) return 0;
+        if (marked_to_exit) goto e;
 
         begin = time_us_32();
         for (a = 0; a < sz; a += 2) {
@@ -116,7 +117,7 @@ int main(void) {
         elapsed = time_us_32() - begin;
         speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "16-bit line read speed: %f MBps\n", speed);
-        if (marked_to_exit) return 0;
+        if (marked_to_exit) goto e;
 
         begin = time_us_32();
         for (a = 0; a < sz; a += 4) {
@@ -125,7 +126,7 @@ int main(void) {
         elapsed = time_us_32() - begin;
         speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "32-bit line write speed: %f MBps\n", speed);
-        if (marked_to_exit) return 0;
+        if (marked_to_exit) goto e;
 
         begin = time_us_32();
         for (a = 0; a < sz; a += 4) {
@@ -137,6 +138,8 @@ int main(void) {
         elapsed = time_us_32() - begin;
         speed = __ddu32_div(__ddu32_mul(d, a), elapsed);
         fgoutf(f, "32-bit line read speed: %f MBps\n", speed);
+    e:
+        free(p);
     }
     return 0;
 }
