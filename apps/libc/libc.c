c99e0853bafaf6828513ee278342756e969d3b45
Mike Vengerovich <mikeveng73@gmail.com>
2025-10-17 04:24:59 +0300

    + w/a's

--- apps/libc/libc.c
+++ apps/libc/libc.c
@@ -1,2 +1,24 @@
+/// POSIX.1
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <sys/stat.h>
+/// libc
+#include <stdio.h>
+#include <string.h>
+
 // W/A TODO: some support for MOS:
 const long _DYNAMIC[1] = { 0 };
+
+int __sys_open(const char* n, int opt1, int opt2) {
+    return open(n, opt1, opt2);
+}
+
+static void log_write(const char* msg) {
+    ///write(STDOUT_FILENO, msg, strlen(msg));
+    printf(msg);
+}
+
+int main() {
+    log_write("It is libc.\n");
+    return -1;
+}
