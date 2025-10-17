2f1397e835d248f1ac85d34c8ce07cf0300d4471
Mike Vengerovich <mikeveng73@gmail.com>
2025-10-17 03:12:35 +0300

    + __sys_call

--- apps/libc/libc.c
+++ apps/libc/libc.c
@@ -1,1 +1,2 @@
-
+// W/A TODO: some support for MOS:
+const long _DYNAMIC[1] = { 0 };
--- libs/musl/src/misc/syscall.c
+++ libs/musl/src/misc/syscall.c
@@ -5,9 +5,21 @@
 
 #undef syscall
 
-long syscall(long n, ...)
-{
-	/* /// TODO:
+#ifndef M_OS_API_SYS_TABLE_BASE
+#define M_OS_API_SYS_TABLE_BASE ((void*)(0x10000000ul + (16 << 20) - (4 << 10)))
+static const unsigned long * const _sys_table_ptrs = (const unsigned long * const)M_OS_API_SYS_TABLE_BASE;
+#endif
+
+typedef void (*goutf_ptr_t)(const char *__restrict str, ...) __attribute__ ((__format__ (__printf__, 1, 2)));
+#define printf(...) ((goutf_ptr_t)_sys_table_ptrs[41])(__VA_ARGS__)
+
+typedef syscall_arg_t (*fn_ptr_t)(syscall_arg_t, syscall_arg_t, syscall_arg_t, syscall_arg_t, syscall_arg_t, syscall_arg_t);
+
+long __syscall(long n, ...) {
+	if (n >= 300) {
+		printf("__syscall(%ph, ...)", n);
+		return 0;
+	}
 	va_list ap;
 	syscall_arg_t a,b,c,d,e,f;
 	va_start(ap, n);
@@ -18,6 +30,25 @@ long syscall(long n, ...)
 	e=va_arg(ap, syscall_arg_t);
 	f=va_arg(ap, syscall_arg_t);
 	va_end(ap);
-	return __syscall_ret(__syscall(n,a,b,c,d,e,f));
-	*/
+	return ((fn_ptr_t)_sys_table_ptrs[n])(a,b,c,d,e,f);
+}
+
+long syscall(long n, ...)
+{
+	if (n >= 300) {
+		printf("syscall(%ph, ...)", n);
+		return 0;
+	}
+	va_list ap;
+	syscall_arg_t a,b,c,d,e,f;
+	va_start(ap, n);
+	a=va_arg(ap, syscall_arg_t);
+	b=va_arg(ap, syscall_arg_t);
+	c=va_arg(ap, syscall_arg_t);
+	d=va_arg(ap, syscall_arg_t);
+	e=va_arg(ap, syscall_arg_t);
+	f=va_arg(ap, syscall_arg_t);
+	va_end(ap);
+	return ((fn_ptr_t)_sys_table_ptrs[n])(a,b,c,d,e,f);
+//	return __syscall_ret(__syscall(n,a,b,c,d,e,f));
 }
